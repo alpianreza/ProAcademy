50ca75f78345c224c23dc19f5148f215372a348a
alpianreza <alpianrezha@gmail.com>
2022-07-21 16:18:13 +0700

    nav drawer and nav component

--- MyNavigationDrawer/app/src/main/java/com/alpianreza/mynavigationdrawer/MainActivity.kt
+++ MyNavigationDrawer/app/src/main/java/com/alpianreza/mynavigationdrawer/MainActivity.kt
@@ -47,7 +47,7 @@ class MainActivity : AppCompatActivity() {
 
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow, R.id.nav_cart
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)

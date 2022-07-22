ee34c6e57bc45ee4ba98f6e0912723b01732e4e3
alpianreza <alpianrezha@gmail.com>
2022-07-22 17:20:39 +0700

    tab layout with viewpager2

---
+++ MyTabLayout/app/src/main/java/com/alpianreza/mytablayout/SectionPagerAdapter.kt
@@ -0,0 +1,21 @@
+package com.alpianreza.mytablayout
+
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class SectionPagerAdapter (activity: AppCompatActivity) : FragmentStateAdapter(activity) {
+    override fun getItemCount(): Int {
+        return 2
+    }
+
+    override fun createFragment(position: Int): Fragment {
+       var fragment: Fragment?= null
+        when (position) {
+            0 -> fragment = HomeFragment()
+            1 -> fragment= ProfileFragment()
+        }
+        return fragment as Fragment
+    }
+
+}

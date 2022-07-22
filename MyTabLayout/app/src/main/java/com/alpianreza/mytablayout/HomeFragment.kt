f5ca8ada871c72887532719ec224d4712ae8c0b5
alpianreza <alpianrezha@gmail.com>
2022-07-22 17:29:46 +0700

    tab layout with one pragment

--- MyTabLayout/app/src/main/java/com/alpianreza/mytablayout/HomeFragment.kt
+++ MyTabLayout/app/src/main/java/com/alpianreza/mytablayout/HomeFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -37,23 +38,16 @@ class HomeFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val tvLabel: TextView= view.findViewById(R.id.section_label)
+        val index= arguments?.getInt(ARG_SECTION_NUMBER, 0)
+
+        tvLabel.text = getString(R.string.content_tab_text, index)
+    }
+
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        const val ARG_SECTION_NUMBER = "section_number"
     }
 }
--- MyTabLayout/app/src/main/java/com/alpianreza/mytablayout/SectionPagerAdapter.kt
+++ MyTabLayout/app/src/main/java/com/alpianreza/mytablayout/SectionPagerAdapter.kt
@@ -1,21 +1,21 @@
 package com.alpianreza.mytablayout
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
 
 class SectionPagerAdapter (activity: AppCompatActivity) : FragmentStateAdapter(activity) {
     override fun getItemCount(): Int {
-        return 2
+        return 3
     }
 
     override fun createFragment(position: Int): Fragment {
-       var fragment: Fragment?= null
-        when (position) {
-            0 -> fragment = HomeFragment()
-            1 -> fragment= ProfileFragment()
+       val fragment= HomeFragment()
+        fragment.arguments = Bundle().apply {
+            putInt(HomeFragment.ARG_SECTION_NUMBER, position + 1)
         }
-        return fragment as Fragment
+        return  fragment
     }
 
 }

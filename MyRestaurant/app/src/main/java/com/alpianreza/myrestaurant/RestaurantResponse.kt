a89d70af64706cba5813e50dd41bcbdfac64375d
alpianreza <alpianrezha@gmail.com>
2022-07-23 20:38:22 +0700

    networking with retrofit

---
+++ MyRestaurant/app/src/main/java/com/alpianreza/myrestaurant/RestaurantResponse.kt
@@ -0,0 +1,37 @@
+package com.alpianreza.myrestaurant
+
+import com.google.gson.annotations.SerializedName
+
+data class RestaurantResponse(
+	val restaurant: Restaurant,
+	val error: Boolean,
+	val message: String
+)
+
+data class CustomerReviewsItem(
+	val date: String,
+	val review: String,
+	val name: String
+)
+
+data class Restaurant(
+	val customerReviews: List<CustomerReviewsItem>,
+	val pictureId: String,
+	val name: String,
+	val rating: Double,
+	val description: String,
+	val id: String
+)
+
+data class PostReviewResponse (
+	@field:SerializedName("customerReviews")
+	val customerReviews: List<CustomerReviewsItem>,
+
+	@field:SerializedName("error")
+	val error: Boolean,
+
+	@field:SerializedName("message")
+	val message: String
+
+)
+
---
+++ MyRestaurant/app/src/main/java/com/alpianreza/myrestaurant/ReviewAdapter.kt
@@ -0,0 +1,24 @@
+package com.alpianreza.myrestaurant
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class ReviewAdapter(private val listReview: List<String>) : RecyclerView.Adapter<ReviewAdapter.ViewHolder>(){
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    val tvItem: TextView = view.findViewById(R.id.tvItem)
+    }
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int) =
+        ViewHolder(LayoutInflater.from(viewGroup.context).inflate(R.layout.item_review, viewGroup, false))
+
+    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+        viewHolder.tvItem.text = listReview[position]
+    }
+
+    override fun getItemCount() = listReview.size
+
+}

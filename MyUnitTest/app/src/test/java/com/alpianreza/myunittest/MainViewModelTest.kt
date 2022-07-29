03d053d37be00cb5d5261017cdf60173cf4339c2
alpianreza <alpianrezha@gmail.com>
2022-07-29 23:42:06 +0700

    unit test usin JUnit and Mockito

---
+++ MyUnitTest/app/src/test/java/com/alpianreza/myunittest/MainViewModelTest.kt
@@ -0,0 +1,22 @@
+package com.alpianreza.myunittest
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class MainViewModelTest {
+
+    @org.junit.jupiter.api.Test
+    fun getCircumference() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun getSurfaceArea() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun getVolume() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun save() {
+    }
+}
